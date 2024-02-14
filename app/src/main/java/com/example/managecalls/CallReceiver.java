eaabd37babbe04eea993f3d7cf2cc6464ab0b16d
jagalou <jagadish03012002@gmail.com>
2024-02-14 15:24:24 +0530

    Under Process

--- app/src/main/java/com/example/managecalls/CallReceiver.java
+++ app/src/main/java/com/example/managecalls/CallReceiver.java
@@ -4,27 +4,35 @@ import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
 
+import androidx.core.app.ActivityCompat;
+
 public class CallReceiver extends BroadcastReceiver {
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
 
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-        if (state != null && state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+        if (state != null && state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
             answerCall(context);
         }
     }
 
-    @Override
     private void answerCall(Context context) {
         TelecomManager telecomManager = (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE);
 
-        if (telecomManager != null){
-            telecomManager.acceptRingingCall();
+        if (telecomManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ANSWER_PHONE_CALLS) == PackageManager.PERMISSION_GRANTED) {
+                    telecomManager.acceptRingingCall();
+                }
+            }
         }
     }
 }
